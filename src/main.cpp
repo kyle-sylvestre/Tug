7b3c59bd214a0749b9b792bd3f1af0e8edacd339
kyle-sylvestre <kyles3133@gmail.com>
2024-04-13 22:17:49 -0400

    add functions from imgui_stdlib.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,54 @@
 #include <imgui_file_window.h>
 #include "liberation_mono.h"
 
+// imgui/misc/imgui_stdlib.cpp
+namespace ImGui
+{
+    struct InputTextCallback_UserData
+    {
+        String*                 Str;
+        ImGuiInputTextCallback  ChainCallback;
+        void*                   ChainCallbackUserData;
+    };
+
+    static int InputTextCallback(ImGuiInputTextCallbackData* data)
+    {
+        InputTextCallback_UserData* user_data = (InputTextCallback_UserData*)data->UserData;
+        if (data->EventFlag == ImGuiInputTextFlags_CallbackResize)
+        {
+            // Resize string callback
+            // If for some reason we refuse the new length (BufTextLen) and/or capacity (BufSize) we need to set them back to what we want.
+            String* str = user_data->Str;
+            IM_ASSERT(data->Buf == str->c_str());
+            str->resize(data->BufTextLen);
+            data->Buf = (char*)str->c_str();
+        }
+        else if (user_data->ChainCallback)
+        {
+            // Forward to user callback, if any
+            data->UserData = user_data->ChainCallbackUserData;
+            return user_data->ChainCallback(data);
+        }
+        return 0;
+    }
+
+    bool InputText(const char* label, 
+                   String* str, 
+                   ImGuiInputTextFlags flags = 0, 
+                   ImGuiInputTextCallback callback = NULL, 
+                   void* user_data = NULL)
+    {
+        IM_ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
+        flags |= ImGuiInputTextFlags_CallbackResize;
+
+        InputTextCallback_UserData cb_user_data = {};
+        cb_user_data.Str = str;
+        cb_user_data.ChainCallback = callback;
+        cb_user_data.ChainCallbackUserData = user_data;
+        return InputText(label, (char*)str->c_str(), str->capacity() + 1, flags, InputTextCallback, &cb_user_data);
+    }
+}
+
 #include <errnoname.c>
 // get error macro name and description
 // ex: ENOENT No such file or directory
